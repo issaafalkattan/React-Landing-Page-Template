c06662427efe75a53af46751b27c5bc5dddd7fd0
DIego Alejandro <diegoalejandrogutierrezs@gmail.com>
2020-09-30 16:38:08 +0000

    Breve descripción de lo realizado

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -19,7 +19,7 @@ export class Navigation extends Component {
               <span className="icon-bar"></span>{" "}
             </button>
             <a className="navbar-brand page-scroll" href="#page-top">
-              React Landing Page
+              Distribuciones Roa Peña
             </a>{" "}
           </div>
 
