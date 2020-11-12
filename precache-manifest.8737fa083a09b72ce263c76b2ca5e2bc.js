f33ecef6679bdc284b5122c19d829643a73c4b8a
sophi-e <sophierithous@gmail.com>
2020-11-12 21:42:32 +0100

    Updates

---
+++ precache-manifest.8737fa083a09b72ce263c76b2ca5e2bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6d72e79c4a10c69fcbf50b36d54988",
+    "url": "/Latechcom/React-Landing-Page-Template.git/index.html"
+  },
+  {
+    "revision": "f36b709a2f2e9b862d71",
+    "url": "/Latechcom/React-Landing-Page-Template.git/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "4946aaebcb49cb194790",
+    "url": "/Latechcom/React-Landing-Page-Template.git/static/js/2.101fb2a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Latechcom/React-Landing-Page-Template.git/static/js/2.101fb2a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36b709a2f2e9b862d71",
+    "url": "/Latechcom/React-Landing-Page-Template.git/static/js/main.3744bdd0.chunk.js"
+  },
+  {
+    "revision": "1237cc054b05edfe93ba",
+    "url": "/Latechcom/React-Landing-Page-Template.git/static/js/runtime-main.ba201dd5.js"
+  }
+]);
