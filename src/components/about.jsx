c5b76a19e8630cefa66823f326c63790b5c0c29a
Santiago8888 <santiago.marti67@gmail.com>
2020-11-14 11:33:10 -0600

    No columns on why us?

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,15 +13,9 @@ export class about extends Component {
                 <p>{this.props.data ? this.props.data.paragraph : 'loading...'}</p>
                 <h3>Why Choose Us?</h3>
                 <div className="list-style">
-                  <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <div className="col-xs-12">
                     <ul>
                       {this.props.data ? this.props.data.Why.map((d, i) => <li  key={`${d}-${i}`}>{d}</li>) : 'loading'}
-                    </ul>
-                  </div>
-                  <div className="col-lg-6 col-sm-6 col-xs-12">
-                    <ul>
-                    {this.props.data ? this.props.data.Why2.map((d, i) => <li  key={`${d}-${i}`}> {d}</li>) : 'loading'}
-
                     </ul>
                   </div>
                 </div>
