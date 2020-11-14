711e3bac0452f02a260fd083273f143eb39d2695
Santiago8888 <santiago.marti67@gmail.com>
2020-11-14 11:41:58 -0600

    Testimonials

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -6,7 +6,7 @@ export class testimonials extends Component {
       <div id="testimonials">
         <div className="container">
           <div className="section-title text-center">
-            <h2>What our clients say</h2>
+            <h2>Who are our ideal clients?</h2>
           </div>
           <div className="row">
             {this.props.data
