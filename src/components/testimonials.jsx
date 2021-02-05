854863aae13cad0076eb256929f04cb836c2ef42
elpidaguy <exabytes.js@gmail.com>
2021-02-05 16:46:28 +0530

    Added Animations for Each Sections
    Added react-reveal library and animated all sections with suitable animations.

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -1,33 +1,36 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 
 export class testimonials extends Component {
   render() {
     return (
-      <div id="testimonials">
-        <div className="container">
-          <div className="section-title text-center">
-            <h2>What our clients say</h2>
-          </div>
-          <div className="row">
-            {this.props.data
-              ? this.props.data.map((d, i) => (
-                  <div key={`${d.name}-${i}`} className="col-md-4">
-                    <div className="testimonial">
-                      <div className="testimonial-image">
-                        {" "}
-                        <img src={d.img} alt="" />{" "}
-                      </div>
-                      <div className="testimonial-content">
-                        <p>"{d.text}"</p>
-                        <div className="testimonial-meta"> - {d.name} </div>
+      <Fade bottom>
+        <div id="testimonials">
+          <div className="container">
+            <div className="section-title text-center">
+              <h2>What our clients say</h2>
+            </div>
+            <div className="row">
+              {this.props.data
+                ? this.props.data.map((d, i) => (
+                    <div key={`${d.name}-${i}`} className="col-md-4">
+                      <div className="testimonial">
+                        <div className="testimonial-image">
+                          {" "}
+                          <img src={d.img} alt="" />{" "}
+                        </div>
+                        <div className="testimonial-content">
+                          <p>"{d.text}"</p>
+                          <div className="testimonial-meta"> - {d.name} </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))
-              : "loading"}
+                  ))
+                : "loading"}
+            </div>
           </div>
         </div>
-      </div>
+      </Fade>
     );
   }
 }
