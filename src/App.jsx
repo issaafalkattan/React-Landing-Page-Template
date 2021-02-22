f97c36cb04ba45098f0d8794b8356d36d2508ba8
Roshith R <r.roshith@gmail.com>
2021-02-22 14:36:42 +0530

    Converted About component to functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from 'react'
-import Navigation from './components/navigation';
-import Header from './components/header';
-import Features from './components/features';
-import About from './components/about';
-import Services from './components/services';
-import Gallery from './components/gallery';
-import Testimonials from './components/testimonials';
-import Team from './components/Team';
-import Contact from './components/contact';
-import JsonData from './data/data.json';
+import Navigation from './components/navigation'
+import Header from './components/header'
+import Features from './components/features'
+import { About } from './components/about'
+import Services from './components/services'
+import Gallery from './components/gallery'
+import Testimonials from './components/testimonials'
+import Team from './components/Team'
+import Contact from './components/contact'
+import JsonData from './data/data.json'
 
 export class App extends Component {
   state = {
     landingPageData: {},
   }
   getlandingPageData() {
-    this.setState({landingPageData : JsonData})
+    this.setState({ landingPageData: JsonData })
   }
 
   componentDidMount() {
-    this.getlandingPageData();
+    this.getlandingPageData()
   }
 
   render() {
@@ -39,4 +39,4 @@ export class App extends Component {
   }
 }
 
-export default App;
+export default App
