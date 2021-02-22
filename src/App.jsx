6fd628e7b6dc2e78bd0d99383422479acfb42524
Roshith R <r.roshith@gmail.com>
2021-02-22 14:51:02 +0530

    Refactored Header component changed Header component to functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Navigation from './components/navigation'
-import Header from './components/header'
+import { Header } from './components/header'
 import { Features } from './components/features'
 import { About } from './components/about'
 import { Services } from './components/services'
