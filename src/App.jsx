78aa61d55d544175611c32d8c2de397f92fc6cf8
Roshith R <r.roshith@gmail.com>
2021-02-22 14:49:43 +0530

    Refactored Features component changed Features component to functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Navigation from './components/navigation'
 import Header from './components/header'
-import Features from './components/features'
+import { Features } from './components/features'
 import { About } from './components/about'
 import { Services } from './components/services'
 import { Gallery } from './components/gallery'
