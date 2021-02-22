3d12ca74677d63bfafb5397ebd30dbca4fa2b713
Roshith R <r.roshith@gmail.com>
2021-02-22 21:29:25 +0530

    Added email js for sending Email added emailjs to send email rather than jquery mailing

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,37 @@
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+
+const initialState = {
+  name: '',
+  email: '',
+  message: '',
+}
 export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setState((prevState) => ({ ...prevState, [name]: value }))
+  }
+  const clearState = () => setState({ ...initialState })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email, message)
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          clearState()
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
   return (
     <div>
       <div id='contact'>
@@ -12,16 +45,18 @@ export const Contact = (props) => {
                   get back to you as soon as possible.
                 </p>
               </div>
-              <form name='sentMessage' id='contactForm' noValidate>
+              <form name='sentMessage' validate onSubmit={handleSubmit}>
                 <div className='row'>
                   <div className='col-md-6'>
                     <div className='form-group'>
                       <input
                         type='text'
                         id='name'
+                        name='name'
                         className='form-control'
                         placeholder='Name'
-                        required='required'
+                        required
+                        onChange={handleChange}
                       />
                       <p className='help-block text-danger'></p>
                     </div>
@@ -31,9 +66,11 @@ export const Contact = (props) => {
                       <input
                         type='email'
                         id='email'
+                        name='email'
                         className='form-control'
                         placeholder='Email'
-                        required='required'
+                        required
+                        onChange={handleChange}
                       />
                       <p className='help-block text-danger'></p>
                     </div>
@@ -47,6 +84,7 @@ export const Contact = (props) => {
                     rows='4'
                     placeholder='Message'
                     required
+                    onChange={handleChange}
                   ></textarea>
                   <p className='help-block text-danger'></p>
                 </div>
