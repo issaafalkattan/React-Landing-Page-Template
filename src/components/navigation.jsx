5611b1df5ec8c8eb8b8d5a14e6cd7fcdaf2b2540
Ayush Kumar <kumarayush7767@gmail.com>
2021-06-07 15:07:09 +0530

    i have added some extra features

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,62 +1,62 @@
 export const Navigation = (props) => {
   return (
-    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
-      <div className='container'>
-        <div className='navbar-header'>
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
           <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
           >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
           </button>
-          <a className='navbar-brand page-scroll' href='#page-top'>
-            React Landing Page
-          </a>{' '}
+          <a className="navbar-brand page-scroll" href="#page-top">
+            <span className="ayush">Geo</span>-Magnify
+          </a>{" "}
         </div>
 
         <div
-          className='collapse navbar-collapse'
-          id='bs-example-navbar-collapse-1'
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
         >
-          <ul className='nav navbar-nav navbar-right'>
+          <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href='#features' className='page-scroll'>
+              <a href="#features" className="page-scroll">
                 Features
               </a>
             </li>
             <li>
-              <a href='#about' className='page-scroll'>
+              <a href="#about" className="page-scroll">
                 About
               </a>
             </li>
             <li>
-              <a href='#services' className='page-scroll'>
+              <a href="#services" className="page-scroll">
                 Services
               </a>
             </li>
             <li>
-              <a href='#portfolio' className='page-scroll'>
+              <a href="#portfolio" className="page-scroll">
                 Gallery
               </a>
             </li>
             <li>
-              <a href='#testimonials' className='page-scroll'>
+              <a href="#testimonials" className="page-scroll">
                 Testimonials
               </a>
             </li>
             <li>
-              <a href='#team' className='page-scroll'>
+              <a href="#team" className="page-scroll">
                 Team
               </a>
             </li>
             <li>
-              <a href='#contact' className='page-scroll'>
+              <a href="#contact" className="page-scroll">
                 Contact
               </a>
             </li>
@@ -64,5 +64,5 @@ export const Navigation = (props) => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
