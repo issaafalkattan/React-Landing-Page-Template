622af4a6e08359ff603f518602776382956aaf4a
Ayush Kumar <kumarayush7767@gmail.com>
2021-06-18 11:54:45 +0530

    i have handled 404 error by adding HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter,HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
