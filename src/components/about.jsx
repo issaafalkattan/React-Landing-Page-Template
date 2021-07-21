5dbccec016c794dad765dd31bbb61f6c7611ec4f
Justinnn07 <justinv2006@gmail.com>
2021-07-21 21:09:41 +0530

    fixed responsiveness :fire:

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,34 +1,34 @@
 export const About = (props) => {
   return (
-    <div id='about'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-xs-12 col-md-6'>
-            {' '}
-            <img src='img/about.jpg' className='img-responsive' alt='' />{' '}
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
           </div>
-          <div className='col-xs-12 col-md-6'>
-            <div className='about-text'>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
               <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : 'loading...'}</p>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
               <h3>Why Choose Us?</h3>
-              <div className='list-style'>
-                <div className='col-lg-6 col-sm-6 col-xs-12'>
+              <div className="list-style">
+                <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Why.map((d, i) => (
                           <li key={`${d}-${i}`}>{d}</li>
                         ))
-                      : 'loading'}
+                      : "loading"}
                   </ul>
                 </div>
-                <div className='col-lg-6 col-sm-6 col-xs-12'>
+                <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Why2.map((d, i) => (
                           <li key={`${d}-${i}`}> {d}</li>
                         ))
-                      : 'loading'}
+                      : "loading"}
                   </ul>
                 </div>
               </div>
@@ -37,5 +37,5 @@ export const About = (props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
