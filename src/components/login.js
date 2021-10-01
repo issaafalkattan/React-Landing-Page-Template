fc99e16221e68640226d4e2b645d48aeb6ba9fc7
Kevin Wu <kwu78@emory.edu>
2021-10-01 16:55:28 -0400

    finish login styling

--- src/components/login.js
+++ src/components/login.js
@@ -3,6 +3,8 @@ import React, { useState, useContext } from "react";
 // import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import Box from '@material-ui/core/Box';
+
 import TextField from "@material-ui/core/TextField";
 import FormGroup from "@material-ui/core/FormGroup";
 // import Card from "@material-ui/core/Card";
@@ -26,40 +28,41 @@ function Login() {
   return (
 
     <div className="login">
-<div class="bglogin"></div>
+<div className="bglogin"></div>
     <p>Arc</p>
-   <Card className="loginCard" bg='light' >
-    <Form>
-  <Form.Group className="mb-3 textfield" controlId="formBasicEmail">
+   <div className="loginCard" bg='light' >
+   <div className="loginCardbg"></div>
+    <form>
+  <Form.Group className="mb-3 logingroup textfield" controlId="formBasicEmail">
   <h1 className="logintitle">
                 {isRegistered ? "Log In" : "Register"}
               </h1>
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
+    {/* <Form.Label className="loginsubtitle">Email address</Form.Label> */}
+    <Form.Control style={{borderRadius:"10px"}} className="logininput" size="lg" as='input' htmlSize='3' type="email" placeholder="Enter email" />
+    <Form.Text className="texto">
       We'll never share your email with anyone else.
     </Form.Text>
   </Form.Group>
 
-  <Form.Group className="mb-3 textfield" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+  <Form.Group className="mb-3 logingroup textfield" controlId="formBasicPassword">
+    {/* <Form.Label>Password</Form.Label> */}
+    <Form.Control style={{borderRadius:"10px"}} className="logininput" size="lg" as='input' type="password" placeholder="Password" />
   </Form.Group>
   <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Click this button to agree on the disclaimer" />
+    <Form.Check type="checkbox" style={{color:"#1f5bca"}}label="Click this button to agree on the disclaimer" />
   </Form.Group>
   <div className="d-grid gap-2">
-  <Button  variant="primary" type="submit" size="lg">
-  {isRegistered ? "Login" : "Register"}
+  <Button  style={{fontSize:"1.5rem"}} className="roundCorner" id="test" variant="primary" type="submit" size="lg">
+  {isRegistered ? "Log In" : "Register"}
   </Button>
   <div className="link">
-                  <Link onClick={handleClicked}>
+                  <Link style={{fontSize:"1.2rem"}} onClick={handleClicked}>
                     {isRegistered ? "Register" : "Login"}
                   </Link>
                 </div>
                 </div>
-</Form>
-</Card>
+</form>
+</div>
     </div>
   );
 }
--- src/home.jsx
+++ src/home.jsx
@@ -26,15 +26,9 @@ const Home = () => {
 
   return (
     <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
+      {/* <Navigation /> */}
       <Gallery data={landingPageData.Gallery}/>
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
+
     </div>
   );
 };
