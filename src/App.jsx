e4cffbc5c05bffdd0c5dad8b14081b555bad55d4
Azhara Kalbaeva <az.kalbaeva@gmail.com>
2022-06-19 22:26:51 +0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,12 @@ const App = () => {
     <div>
       <Navigation />
       <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
+      {/*<Features data={landingPageData.Features} />*/}
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery}/>
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
+      <Gallery data={landingPageData.Gallery} />
+      {/*<Testimonials data={landingPageData.Testimonials} />*/}
+      {/*<Team data={landingPageData.Team} />*/}
       <Contact data={landingPageData.Contact} />
     </div>
   );
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,7 @@ export const About = (props) => {
         <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            <img src="img/about.jpeg" className="img-responsive" alt="" />{" "}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
