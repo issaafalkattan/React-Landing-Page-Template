ec8a07152a5d35b693d8a17d417408d060e1735f
Pedro Marques <ppmarq1@gmail.com>
2023-03-17 12:09:42 +0000

    Update content

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+          <img src="img/Sambo-logo.png" className="img-responsive" alt="" />{" "}
           </a>{" "}
         </div>
 
