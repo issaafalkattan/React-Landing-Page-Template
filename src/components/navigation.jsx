2ca822cb68e498947e505d04d5e1dd728a887405
spamdalfz <coreyvasser@gmail.com>
2023-12-13 23:47:58 -0800

    modified:   src/data/data.json

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+            GrayC Electric
           </a>{" "}
         </div>
 
