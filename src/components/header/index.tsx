5e7299088f6987d06e23801c423abec9c5f20a9f
乔方圆 <qiaofangyuan@qiaofangyuandeMacBook-Pro.local>
2024-02-02 11:49:34 +0800

    fix: 移动端适配

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <div className="top-bar">
-        <img src="/logo.jpg" width={30} height={30} alt="ccai logo" />
+        <img src="/logo.png" width={50} height={50} alt="ccai logo" />
         <div className="left">草船科技</div>
       </div>
     </header>
